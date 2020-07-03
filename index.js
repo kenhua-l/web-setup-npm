4684a6e01e6150cf0afb532e43a052fa61a5f555
kenhua-l <l_kenhua93@hotmail.com>
2020-07-03 23:20:43 +0800

    add test

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 exports.printMsg = function() {
   console.log("this is a demo");
 }
+
+class Color {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+}
+
+const allColors = [
+  new Color('brightred', '#e74C3c'),
+]
+
+exports.getRandomColor = () => {
+  return allColors[Math.floor(Math.random() * allColors.length)];
+}
+
+exports.allColors = allColors;
